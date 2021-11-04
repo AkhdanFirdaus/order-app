1a5f1be16cc730b72813f2b08e4b083c24b6842a
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2021-11-04 12:59:11 +0700

    Membuat halaman login

---
+++ app/src/main/java/com/akhdanfirdaus/orderin/MainActivity.java
@@ -0,0 +1,38 @@
+package com.akhdanfirdaus.orderin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView usernameView = findViewById(R.id.username_edt);
+        TextView passwordView = findViewById(R.id.password_edt);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(view -> {
+            String username = usernameView.getText().toString();
+            String password = passwordView.getText().toString();
+
+            if (username.equals("akhdan") && password.equals("password")) {
+                //Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                //startActivity(intent);
+                this.finish();
+            } else {
+                Toast.makeText(getApplicationContext(), "Wrong Username/Password", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
