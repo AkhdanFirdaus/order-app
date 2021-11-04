b23047e7a6ffc7d087e9f3f007e56cecb8f15c50
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2021-11-04 13:40:37 +0700

    Base home layout dan Menu

--- app/src/main/java/com/akhdanfirdaus/orderin/HomeActivity.java
+++ app/src/main/java/com/akhdanfirdaus/orderin/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.akhdanfirdaus.orderin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_action_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout_menu:
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                this.finish();
+                break;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
